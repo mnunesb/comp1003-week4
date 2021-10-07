2b05301a88261da0bf5904864255fe40bc4c05e4
Marcello Nunes Bernardes <mnunesbernardes@gmail.com>
2021-10-07 14:26:25 -0300

    Added php

--- Lesson Files Start/process-form.php
+++ Lesson Files Start/process-form.php
@@ -10,20 +10,29 @@
 		<h2>Payment Information Updated</h2>
 <?php
 	/* STEP 1: Capture all the user-inputted form data from each of the FORM elements sent by index.php with appropriate variable names - use the value of the name attribute inside the $_GET array (we set the form method to "get") */
-
+	$fname = $_GET['fname'];
+	$lname = $_GET['lname'];
+	$phone = $_GET['phone'];
+	$email = $_GET['email'];
+	$ccType = $_GET['ccType'];
+	$ccYear = $_GET['ccYear'];
+	$ccMonth = $_GET['ccMonth'];
+	$commPrefs = $_GET['comm'];
 
 	// STEP 2a: Output a friendly message with a paragraph (P) to confirm that everything went well, by stringing together (concatenating) the above form data.
-
+	echo('<p>Thank you, '.$fname.' '.$lname.',your form was submited.</p>');
 
 
 	// STEP 2b: Use an unordered list element (UL and LI) to display the other setting values.
 
-	echo('<p>Your communication settings include notifications by the following:</p>
-<ul>');
-	// STEP 3: The checkboxes for Communications Preferences will be sent as an array (as there could be more than one option checked by the user - so we need to loop through each item with FOREACH).
-
-	echo('</ul>
-<p>Hava a great day.</p>');
+ 	echo('<p>Your communication settings include notifications by the following:</p>');
+	echo("<ul>
+			<li>Phone: $phone</li>
+			<li>Email: $email</li>
+			<li>Credit Card: $ccType</li>
+			<li>Expires (year/month): $ccYear/$ccMonth'</li>
+		</ul>
+		<p>Have a great day.</p>");
 	// STEP 4: Upload this file to the server, enter data into your form, and hit the submit button to try it.
 ?>
 	<!-- STEP 5: Head back to STEP 13 of index.php and we will do some simple form data validation -->
